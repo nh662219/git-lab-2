4caf04d3832f262500fe6e9153dc463649d4a92e
Nathan Hoffman <nh662219@ohio.edu>
2022-01-18 10:48:12 -0500

    Committed git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Nathan Hoffman>
+ *        Date: <1\18\2022>
+ * Description: git-lab-2 C++ program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
